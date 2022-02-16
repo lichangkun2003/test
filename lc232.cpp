de973d55e2565df9c99bd417a9d857ee7965e65a
lichangkun2003 <1121974023@qq.com>
2022-02-16 11:28:39 +0800

    练习

---
+++ lc232.cpp
@@ -0,0 +1,56 @@
+#include <stack>
+#include <vector>
+using namespace std;
+
+class MyQueue {
+private:
+    stack<int> stk1 , stk2;
+public:
+    MyQueue() {
+
+    }
+    
+    void push(int x) {
+        stk1.push( x );
+    }
+    
+    int pop() {
+        while( !stk1.empty() ){
+            stk2.push( stk1.top() );
+            stk1.pop();
+        }
+        int ret = stk2.top();
+        stk2.pop();
+        while( !stk2.empty() ){
+            stk1.push( stk2.top() );
+            stk2.pop();
+        }
+        return ret;
+    }
+    
+    int peek() {
+        while( !stk1.empty() ){
+            stk2.push( stk1.top() );
+            stk1.pop();
+        }
+        int ret = stk2.top();
+        while( !stk2.empty() ){
+            stk1.push( stk2.top() );
+            stk2.pop();
+        }
+        return ret;
+    }
+    
+    bool empty() {
+        return stk1.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
