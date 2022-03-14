25154cce8aa5e40f9d7bc1a79167bb290dc2d601
lichangkun2003 <1121974023@qq.com>
2022-03-14 21:35:54 +0800

    练习

---
+++ lc599.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        int n = list1.size();
+        unordered_map <string , int > index;
+        for( int i = 0 ; i < n ; i++ )
+            index[ list1[i] ] = i;
+        int sum = INT_MAX;
+        vector<string> ans;
+        for( int i = 0 ; i < list2.size() ; ++i ){
+            if( index.count( list2[i] ) > 0 ){
+                int j = index[ list2[i] ];
+                if( j + i < sum ){
+                    ans.clear();
+                    ans.push_back( list2[i] );
+                    sum = j + i;
+                }
+                else if( i + j == sum )
+                    ans.push_back( list2[i] );
+            }
+        }
+        return ans;
+
+    }
+};
+
+
+
+// class Solution {
+// public:
+//     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+//         unordered_map<string, int> index;
+//         for (int i = 0; i < list1.size(); i++) {
+//             index[list1[i]] = i;
+//         }
+
+//         vector<string> ret;
+//         int indexSum = INT_MAX;
+//         for (int i = 0; i < list2.size(); i++) {
+//             if (index.count(list2[i]) > 0) {
+//                 int j = index[list2[i]];
+//                 if (i + j < indexSum) {
+//                     ret.clear();
+//                     ret.push_back(list2[i]);
+//                     indexSum = i + j;
+//                 } else if (i + j == indexSum) {
+//                     ret.push_back(list2[i]);
+//                 }
+//             }
+//         }
+//         return ret;
+//     }
+// };
