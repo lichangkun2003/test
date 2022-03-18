30f32143f0af5f9d932caca86cfd36ccd4d49e48
lichangkun2003 <1121974023@qq.com>
2022-03-18 19:21:09 +0800

    练习

---
+++ lc389.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int cnt[26] = {0};
+        for( int i = 0 ; i < s.size() ; ++i ){
+            cnt[ s[i] - 'a' ]++;
+        }
+        for( int i = 0 ; i < t.size() ; ++i ){
+            cnt[ t[i] - 'a' ]--;
+            if( cnt[ t[i] - 'a'] < 0 )
+                return t[i];
+        }
+        return '\0';
+    }
+};
