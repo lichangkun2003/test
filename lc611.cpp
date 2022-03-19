f4fcc1187fe0337041897b232f85616e58fc9806
lichangkun2003 <1121974023@qq.com>
2022-03-19 14:18:35 +0800

    练习

---
+++ lc611.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort( nums.begin() , nums.end() );
+        int n = nums.size() , cnt = 0;
+        for( int i = 0 ; i < n ; i++ ){
+            for( int j = i + 1 ; j < n ; j++){
+                int left = j + 1 , right = n - 1 , k = j;
+                while( left <= right){
+                    int mid = ( left + right ) / 2;
+                    if( nums[mid] < nums[i] + nums[j] ){
+                        k = mid;
+                        left = mid + 1;
+                    }
+                    else    
+                        right = mid - 1;
+                }
+                cnt += k - j;
+            }
+        }
+        return cnt;
+    }
+};
