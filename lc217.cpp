f4157175069049b01da65d93daed7daebde6e7d5
lichangkun2003 <1121974023@qq.com>
2022-03-22 21:54:18 +0800

    练习

---
+++ lc217.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for( int x : nums ){
+            if( s.find( x ) != s.end() )
+                return true;
+            s.insert( x );
+        }
+        return false;
+    }
+};
