1a3d3ffb30acb7583012f7859e30cd812d360e45
lichangkun2003 <1121974023@qq.com>
2022-04-19 21:35:29 +0800

    练习

---
+++ lc404.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <math.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        queue< TreeNode* > q;
+        if( !root )
+            return 0;
+        q.push( root );
+        int ans = 0;
+        while( !q.empty() ){
+            TreeNode* cur = q.front();
+            q.pop();
+            if( cur->left ){
+                if( !cur->left->left && !cur->left->right )
+                    ans += cur->left->val;
+                q.push( cur->left );
+            }
+            if( cur->right )
+                q.push( cur->right );
+        }
+        return ans;
+    }
+};
